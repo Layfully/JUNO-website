31fd1369bf860773cd004b2dc7695ec480ca290a
Adrian Gaborek <adriangaborek3@gmail.com>
2021-01-18 21:20:06 +0100

    Get rid of unused styles, configure tailwind to use two fonts 'Alegreya SC and Lekton', extend tailwind with orange color definition, remove unnecesary css resets and include ul styles.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['Lekton', ...defaultTheme.fontFamily.mono]
+      },
+      colors: {
+        orange: '#FF7A00'
+      }
+    },
+    fontFamily: {
+      title: ['Alegreya SC', ...defaultTheme.fontFamily.sans]
+    }
   },
   variants: {
     extend: {},
