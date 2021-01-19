e6ce297f6c7078ef11f782e6415d281be491947b
Adrian Gaborek <adriangaborek3@gmail.com>
2021-01-19 13:49:17 +0100

    Add custom underline for titles

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title, "text-4xl text-white text-opacity-50 font-title"}>
-          J/ψ Under Observation ...
+          J/ψ <span className="custom-underline">U</span>nder Observation ...
         </h1>
         <h2 className="text-2xl text-orange text-opacity-60 font-mono">Physics story explained easy</h2>
 
@@ -30,12 +30,12 @@ intends to excite people for the fascinating world of particle physics.</li>
 
       <footer className={styles.footer, "wave-container text-center relative"}>
         <div className="absolute bottom-0">
-          <svg svg width="1920" height="1200" viewBox="0 0 1920 1200" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <svg width="1920" height="1200" viewBox="0 0 1920 1200" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M1354.39 200.168C1297.9 14.5135 687.099 10.809 -1 0V1200H2018.1L2299 1131.94C2270.49 1022.02 2174.39 785.955 2018.1 721.101C1861.81 656.247 1615.23 951.795 1223.45 818.684C870.74 716.599 1410.89 385.823 1354.39 200.168Z" fill="#231F1F" />
           </svg>
         </div>
         <div className="relative px-8">
-          <h1 className="text-6xl text-white text-opacity-50 font-mono font-bold mb-16" >Learn more about: </h1>
+          <h1 className="text-6xl text-white text-opacity-50 font-mono font-bold mb-16 " ><span className="custom-underline">Le</span>arn more about:</h1>
           <h2 className="text-4xl text-white text-opacity-50 font-mono font-bold my-8">World of particle physics</h2>
           <ul className="list-bullet text-left ">
             <li className="text-2xl text-white text-opacity-50 font-mono">Particle Physics at CERN</li>
