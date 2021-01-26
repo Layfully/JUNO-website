0f16e40dd5e5ed1d1a1a42673234937a0780d266
Adrian Gaborek <adriangaborek3@gmail.com>
2021-01-26 19:30:39 +0100

    add polyfill for smoothscroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import smoothscroll from 'smoothscroll-polyfill';
+
+// kick off the polyfill!
+smoothscroll.polyfill();
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
