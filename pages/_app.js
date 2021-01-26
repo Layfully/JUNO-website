22ffd6bf9496f27fbafa91d4c78064f3fabe7405
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2021-01-26 21:36:21 +0100

    add smooth scroll (#18)
    * add smooth scroll
    
    * add polyfill for smooth scrolling
    
    * tested on browsers that required polyfill

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
+import { useEffect } from 'react'
+import smoothscroll from 'smoothscroll-polyfill';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof (window) != 'undefined') {
+      smoothscroll.polyfill();
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
