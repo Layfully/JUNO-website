49675337c1558a0d040d3879fb70c49dbee0f34f
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2021-02-13 14:03:43 +0100

    apply prettier to everything (#30)
    Co-authored-by: Adrian Gaborek <adriangaborek3@gmail.com>

--- components/HomeSection/HomeSection.js
+++ components/HomeSection/HomeSection.js
@@ -1,54 +1,64 @@
-import Image from 'next/image'
-import Text from '../Text'
-import React from 'react'
-import styles from './HomeSection.module.css'
-import sectionStyles from '../../styles/Home.module.css'
-
+import Image from "next/image";
+import Text from "../Text";
+import React from "react";
+import styles from "./HomeSection.module.css";
+import sectionStyles from "../../styles/Home.module.css";
 
 class HomeSection extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            images: [
-                {
-                    src: '/d-meson.png',
-                    alt: 'd-meson',
-                    width: 1130,
-                    height: 1200
-                },
-                {
-                    src: '/j-psi.png',
-                    alt: 'j/psi',
-                    width: 537,
-                    height: 1824
-                },
-                {
-                    src: '/keon.png',
-                    alt: 'keon',
-                    width: 1354,
-                    height: 1042
-                }
-            ]
-        }
-    }
+    this.state = {
+      images: [
+        {
+          src: "/d-meson.png",
+          alt: "d-meson",
+          width: 1130,
+          height: 1200,
+        },
+        {
+          src: "/j-psi.png",
+          alt: "j/psi",
+          width: 537,
+          height: 1824,
+        },
+        {
+          src: "/keon.png",
+          alt: "keon",
+          width: 1354,
+          height: 1042,
+        },
+      ],
+    };
+  }
 
-    render() {
-        return (
-            <section id="Home" className={`${sectionStyles.section} flex-col`}>
-                <header className={styles.titleContainer}>
-                    <Text size="h1" custom="font-title">J/ψ <span className={styles.customUnderline}>U</span>nder Observation ...</Text>
-                    <Text size="body" color="orange">The ultimate Particle Physics experience</Text>
-                </header>
-                <div className={styles.particleContainer}>
-                    {
-                        this.state.images.map((image, index) => <Image key={index} src={image.src} alt={image.alt} width={image.width} height={image.height} layout="intrinsic" />)
-                    }
-                </div>
-            </section>
-        )
-    }
-
+  render() {
+    return (
+      <section id='Home' className={`${sectionStyles.section} flex-col`}>
+        <header className={styles.titleContainer}>
+          <Text size='h1' custom='font-title'>
+            J/ψ <span className={styles.customUnderline}>U</span>nder
+            Observation ...
+          </Text>
+          <Text size='body' color='orange'>
+            The ultimate Particle Physics experience
+          </Text>
+        </header>
+        <div className={styles.particleContainer}>
+          {this.state.images.map((image, index) => (
+            <Image
+              key={index}
+              src={image.src}
+              alt={image.alt}
+              width={image.width}
+              height={image.height}
+              layout='intrinsic'
+            />
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
-export default HomeSection
+export default HomeSection;
--- components/ProgressSection/ProgressSection.js
+++ components/ProgressSection/ProgressSection.js
@@ -1,14 +1,12 @@
-import React from 'react'
-import styles from '../../styles/Home.module.css'
-
+import React from "react";
+import styles from "../../styles/Home.module.css";
 
 const ProgressSection = () => (
-    <section id='Progress' className={styles.section}>
-        <div className='text-3xl text-orange text-opacity-60'>
-            Visualization of Data-Acquisition at BNL
-        </div>
-    </section> 
-)
+  <section id='Progress' className={styles.section}>
+    <div className='text-3xl text-orange text-opacity-60'>
+      Visualization of Data-Acquisition at BNL
+    </div>
+  </section>
+);
 
-
-export default ProgressSection
+export default ProgressSection;
