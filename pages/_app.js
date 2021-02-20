8dbdd4da0d2bb2857eb96927ed6f314bb41eac54
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-20 16:09:33 +0100

    allow user scaling

--- pages/_app.js
+++ pages/_app.js
@@ -15,10 +15,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>JUNO</title>
         <link rel='icon' href='/favicon.ico' />
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'
-        />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <Component {...pageProps} />
     </>
