9565ca71f2a3ccab5d92d787dc048af21861078c
Adrian Gaborek <adrian.gaborek@student.up.krakow.pl>
2021-02-21 00:06:24 +0100

    General improvements (#39)
    * change meta viewport
    
    * move head to app
    
    * add particles wrapper
    
    * fix issue with mobile addres bar
    
    * disable builtin resizing
    
    * fix particles canvas size
    
    Co-authored-by: Adrian Gaborek <adriangaborek3@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Menu from "../components/Menu/Menu";
 import Text from "../components/Text";
+import ParticlesWrapper from "../components/ParticlesWrapper";
 import HomeSection from "../components/HomeSection/HomeSection";
 import StorySection from "../components/StorySection/StorySection";
 import TeaserSection from "../components/TeaserSection/TeaserSection";
@@ -11,132 +11,130 @@ import ContactForm from "../components/ContactForm/ContactForm";
 import { FooterBackground } from "../components/Svg";
 import Slider from "../components/Slider/Slider";
 import sectionStyles from "./../styles/Home.module.css";
-import Particles from "react-particles-js";
 
 export default function Home() {
   return (
-    <div className='bg-transparent overflow-x-hidden'>
-      <Particles canvasClassName='bg-black fixed -z-1 min-h-screen' />
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <header>
-        <Menu />
-      </header>
+    <>
+      <ParticlesWrapper />
 
-      <main className={styles.main}>
-        <HomeSection />
-        <StorySection />
-        <TeaserSection />
-        <ProgressSection step={1} />
-        <ContactForm subject='Hello JUNO Team!' />
-        <section id='Ourteam' className={sectionStyles.section}>
-          <Slider />
-        </section>
-        <AffiliatesSection />
-      </main>
+      <div className='bg-transparent overflow-x-hidden'>
+        <header>
+          <Menu />
+        </header>
+        <main className={styles.main}>
+          <HomeSection />
+          <StorySection />
+          <TeaserSection />
+          <ProgressSection step={1} />
+          <ContactForm subject='Hello JUNO Team!' />
+          <section id='Ourteam' className={sectionStyles.section}>
+            <Slider />
+          </section>
+          <AffiliatesSection />
+        </main>
 
-      <footer className={styles.footerContainer} id='About'>
-        <FooterBackground />
-        <div className={styles.footer}>
-          <Text size='h1' custom='font-bold pt-16 pb-8'>
-            <span className={styles.customUnderline}>Le</span>arn more about:
-          </Text>
-          <Text size='h2' custom='font-bold my-8'>
-            World of particle physics
-          </Text>
-          <ul className={`${styles.list} ${styles.listBullet} text-left`}>
-            <li>
-              <Text hover>
-                <a href='https://home.cern/science/physics'>
-                  Particle Physics at CERN
-                </a>
-              </Text>
-            </li>
-            <li>
-              <Text hover>
-                <a href='https://atlas.cern/discover/physics'>
-                  Particle Physics at ALTAS
-                </a>
-              </Text>
-            </li>
-            <li>
-              <Text hover>
-                <a href='https://home.cern/science/physics/standard-model'>
-                  The Standard Model of Particle Physics
-                </a>
-              </Text>
-            </li>
-          </ul>
-          <Text size='h2' custom='font-bold my-8'>
-            Laboratories that discovered J/ψ
-          </Text>
-          <ul className={`${styles.list} ${styles.listBullet} text-left`}>
-            <li>
-              <Text hover>
-                <a href='https://www6.slac.stanford.edu/about/slac-overview'>
-                  SLAC - Stanford Linear Accelerator Center
-                </a>
-              </Text>
-            </li>
-            <li>
-              <Text hover>
-                <a href='https://www6.slac.stanford.edu/about/slac-history'>
-                  History of SLAC
-                </a>
-              </Text>
-            </li>
-            <li>
-              <Text hover>
-                <a href='https://www.bnl.gov/about/w'>
-                  BNL - Brookhaven National Laboratory
-                </a>
-              </Text>
-            </li>
-            <li>
-              <Text hover>
-                <a href='https://www.bnl.gov/about/history/discoveries.php'>
-                  History of discoveries at BNL
-                </a>
-              </Text>
-            </li>
-          </ul>
-          <Text size='h2' custom='font-bold my-8'>
-            Stay up to date:
-          </Text>
-          <div className={styles.linkContainer}>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://www.symmetrymagazine.org/'>Symmetry Magazine</a>
+        <footer className={styles.footerContainer} id='About'>
+          <FooterBackground />
+          <div className={styles.footer}>
+            <Text size='h1' custom='font-bold pt-16 pb-8'>
+              <span className={styles.customUnderline}>Le</span>arn more about:
             </Text>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://www.quantamagazine.org/tag/particle-physics/'>
-                Quanta Maganizne
-              </a>
+            <Text size='h2' custom='font-bold my-8'>
+              World of particle physics
             </Text>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://www.nature.com/subjects/particle-physics'>
-                Nature Research
-              </a>
+            <ul className={`${styles.list} ${styles.listBullet} text-left`}>
+              <li>
+                <Text hover>
+                  <a href='https://home.cern/science/physics'>
+                    Particle Physics at CERN
+                  </a>
+                </Text>
+              </li>
+              <li>
+                <Text hover>
+                  <a href='https://atlas.cern/discover/physics'>
+                    Particle Physics at ALTAS
+                  </a>
+                </Text>
+              </li>
+              <li>
+                <Text hover>
+                  <a href='https://home.cern/science/physics/standard-model'>
+                    The Standard Model of Particle Physics
+                  </a>
+                </Text>
+              </li>
+            </ul>
+            <Text size='h2' custom='font-bold my-8'>
+              Laboratories that discovered J/ψ
             </Text>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://news.fnal.gov/fermilab-at-work/'>
-                Fermilab at Work
-              </a>
+            <ul className={`${styles.list} ${styles.listBullet} text-left`}>
+              <li>
+                <Text hover>
+                  <a href='https://www6.slac.stanford.edu/about/slac-overview'>
+                    SLAC - Stanford Linear Accelerator Center
+                  </a>
+                </Text>
+              </li>
+              <li>
+                <Text hover>
+                  <a href='https://www6.slac.stanford.edu/about/slac-history'>
+                    History of SLAC
+                  </a>
+                </Text>
+              </li>
+              <li>
+                <Text hover>
+                  <a href='https://www.bnl.gov/about/w'>
+                    BNL - Brookhaven National Laboratory
+                  </a>
+                </Text>
+              </li>
+              <li>
+                <Text hover>
+                  <a href='https://www.bnl.gov/about/history/discoveries.php'>
+                    History of discoveries at BNL
+                  </a>
+                </Text>
+              </li>
+            </ul>
+            <Text size='h2' custom='font-bold my-8'>
+              Stay up to date:
             </Text>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://cms.cern/'>CMS</a>
-            </Text>
-            <Text color='orange' size='body-base' hover>
-              <a href='https://particleadventure.org/'>Quiz</a>
+            <div className={styles.linkContainer}>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://www.symmetrymagazine.org/'>
+                  Symmetry Magazine
+                </a>
+              </Text>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://www.quantamagazine.org/tag/particle-physics/'>
+                  Quanta Maganizne
+                </a>
+              </Text>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://www.nature.com/subjects/particle-physics'>
+                  Nature Research
+                </a>
+              </Text>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://news.fnal.gov/fermilab-at-work/'>
+                  Fermilab at Work
+                </a>
+              </Text>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://cms.cern/'>CMS</a>
+              </Text>
+              <Text color='orange' size='body-base' hover>
+                <a href='https://particleadventure.org/'>Quiz</a>
+              </Text>
+            </div>
+            <Text size='body-base' custom='text-center pt-14 pb-4'>
+              2021 &#169; Juno
             </Text>
           </div>
-          <Text size='body-base' custom='text-center pt-14 pb-4'>
-            2021 &#169; Juno
-          </Text>
-        </div>
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   );
 }
