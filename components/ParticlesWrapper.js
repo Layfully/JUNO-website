cf97584aefd3e1aa900a03ed9034969508aefd2e
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-20 16:20:37 +0100

    further test

--- components/ParticlesWrapper.js
+++ components/ParticlesWrapper.js
@@ -3,7 +3,13 @@ import React from "react";
 import useWindowSize from "../hooks/useWindowSize";
 
 const ParticlesWrapper = (props) => {
-  return <Particles canvasClassName='bg-black fixed -z-1' />;
+  return (
+    <Particles
+      canvasClassName='bg-black fixed -z-1 top-0 left-0'
+      width={useWindowSize().width}
+      height={useWindowSize().height}
+    />
+  );
 };
 
 export default ParticlesWrapper;
