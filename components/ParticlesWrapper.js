69d5a547cc15dee5de56cd8ffbaffda24f5e7b69
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-20 22:36:33 +0100

    fix particles canvas size

--- components/ParticlesWrapper.js
+++ components/ParticlesWrapper.js
@@ -1,20 +1,38 @@
 import Particles from "react-particles-js";
 import React from "react";
-import useWindowSize from "../hooks/useWindowSize";
 
-const ParticlesWrapper = (props) => {
-  return (
-    <Particles
-      canvasClassName='bg-black fixed pointer-events-none -z-1 top-0 left-0'
-      params={{
-        interactivity: {
-          events: {
-            resize: false,
+class ParticlesWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 0,
+      width: 0,
+    };
+  }
+
+  componentDidMount() {
+    if (typeof window !== "undefined") {
+      this.setState({ height: window.outerHeight + 200 });
+      this.setState({ width: window.outerWidth });
+    }
+  }
+
+  render() {
+    return (
+      <Particles
+        canvasClassName='bg-black fixed pointer-events-none -z-1 top-0 left-0'
+        height={this.state.height}
+        width={this.state.width}
+        params={{
+          interactivity: {
+            events: {
+              resize: false,
+            },
           },
-        },
-      }}
-    />
-  );
-};
+        }}
+      />
+    );
+  }
+}
 
 export default ParticlesWrapper;
