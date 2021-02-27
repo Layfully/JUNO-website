d59bd191f5758a10985ee6a25f01bf925dc51183
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-28 00:48:04 +0100

    add svg fallback

--- components/LanguageSelector.js
+++ components/LanguageSelector.js
@@ -3,6 +3,40 @@ import React from "react";
 import Text from "../components/Text";
 
 class LanguageSelector extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      supportsEmoji: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ supportsEmoji: this.supportsFlagEmoji(document) });
+  }
+
+  supportsFlagEmoji = () => {
+    if (typeof window !== undefined) {
+      var canvas = document.createElement("canvas");
+      canvas.height = 10;
+      canvas.width = canvas.height * 2;
+      var ctx = canvas.getContext("2d");
+      ctx.font = canvas.height + "px Arial";
+      ctx.fillText("🇬🇧", 0, canvas.height);
+      var data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+      var i = 0;
+      while (i < data.length) {
+        if (data[i] !== data[i + 1] || data[i] !== data[i + 2]) {
+          canvas.remove();
+          return true;
+        }
+        i += 4;
+      }
+      canvas.remove();
+      return false;
+    }
+  };
+
   render() {
     return (
       <div className='inline-block rounded-lg z-10 space-x-5 my-10'>
@@ -16,13 +50,27 @@ class LanguageSelector extends React.Component {
                 ? "text-opacity-100"
                 : "text-opacity-50"
             }`}>
-            <a href={language === "en" ? "/" : `/${language}`}>
-              <ReactCountryFlag
-                countryCode={language === "en" ? "us" : language}
-                className='pr-2'
-              />
-              {language.toUpperCase()}
-            </a>
+            {this.state.supportsEmoji ? (
+              <a href={language === "en" ? "/" : `/${language}`}>
+                <ReactCountryFlag
+                  countryCode={language === "en" ? "us" : language}
+                  className='pr-2'
+                />
+                {language.toUpperCase()}
+              </a>
+            ) : (
+              <a
+                href={language === "en" ? "/" : `/${language}`}
+                className='inline-flex items-center'>
+                <ReactCountryFlag
+                  countryCode={language === "en" ? "us" : language}
+                  className='mr-2'
+                  cdnUrl='https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/flags/4x3/'
+                  svg
+                />
+                {language.toUpperCase()}
+              </a>
+            )}
           </Text>
         ))}
       </div>
