5e59e976cc0055729ba3605e547cc81fa80e5af3
Adrian Gaborek <adriangaborek3@gmail.com>
2021-02-27 13:32:29 +0100

    add x-default hreflang

--- components/MetaHead.js
+++ components/MetaHead.js
@@ -13,6 +13,7 @@ const MetaHead = ({ blok, currentLanguage }) => (
         key={`${language._uid}-short`}
       />
     ))}
+    <link rel='alternate' hreflang='x-default' href={blok.x_default.url} />
     <meta name='author' content={blok.author} />
     <meta name='keywords' content={blok.keywords} />
     <meta name='description' content={blok.description} />
